27184aa3ec608ad033215647a522c5bf73f0bd54
nomo-app <142992428+nomo-app@users.noreply.github.com>
2023-09-11 22:37:51 +0200

    Fix nomoAuthHttp simulation mode (#13)
    * Fix nomoauth simulation mode
    
    * publish

--- nomo-plugin-kit/dist/nomo_auth.js
+++ nomo-plugin-kit/dist/nomo_auth.js
@@ -18,17 +18,37 @@ export async function nomoAuthFetch(args) {
     return await invokeNomoFunction("nomoAuthHttp", args);
 }
 /**
+ * a map from nomo-auth-addr to JWT
+ */
+const _cachedJWTs = {};
+function _injectNomoAuthHeaders({ nomoAuthAddress, url, headers, }) {
+    headers["nomo-auth-addr"] = nomoAuthAddress;
+    headers["nomo-auth-version"] = "1.0.0";
+    headers["nomo-plugin"] = "PluginName/PluginVersion";
+    const jwt = _cachedJWTs[nomoAuthAddress];
+    if (jwt) {
+        headers["nomo-sig"] = _nomoSignMessageSimulation({ jwt, url });
+        headers["Authorization"] = `Bearer ${jwt}`;
+    }
+}
+function _nomoSignMessageSimulation({ jwt, url, }) {
+    console.log("Nomo-Auth: simulate signature for url " + url);
+    // nomoSignMessage is not implemented in simulation-mode, therefore we return a hardcoded signature
+    return "HCaJ9SEvzyRXGbtDmtvZxErBLgyiOGWtAjBwavyWqhaBFsQB4MzjiHgaF9Ia2MA9IOfZ5W/fUC56UXzE96IN6nk=";
+}
+/**
  * This is a browser-simulation of the Nomo-Auth-Protocol.
  */
 export async function simulateNomoAuthHttp(args) {
-    const injectedHeaders = {
-        "nomo-auth-version": "1.0.0",
-        "nomo-auth-addr": "cNpBzxornzED1MsBKDupMbwqZnkFtoUVGD",
-        "nomo-plugin": "PluginName/PluginVersion",
-    };
+    var _a;
+    // We hardcode "nomo-auth-addr" for the simulation mode.
+    // In the real Nomo-App, "nomo-auth-addr" is different for each domain.
+    const nomoAuthAddress = "cNpBzxornzED1MsBKDupMbwqZnkFtoUVGD";
+    const headers = (_a = args.headers) !== null && _a !== void 0 ? _a : {};
+    _injectNomoAuthHeaders({ nomoAuthAddress, url: args.url, headers });
     let res = await fetch(args.url, {
         method: args.method,
-        headers: Object.assign(Object.assign({}, injectedHeaders), args.headers),
+        headers,
         body: args.body,
     });
     let statusCode = res.status;
@@ -40,12 +60,11 @@ export async function simulateNomoAuthHttp(args) {
         if (!jwt) {
             return Promise.reject("got 403 but missing JWT");
         }
-        injectedHeaders["Authorization"] = "Bearer " + jwt;
-        injectedHeaders["nomo-sig"] =
-            "HCaJ9SEvzyRXGbtDmtvZxErBLgyiOGWtAjBwavyWqhaBFsQB4MzjiHgaF9Ia2MA9IOfZ5W/fUC56UXzE96IN6nk=";
+        _cachedJWTs[nomoAuthAddress] = jwt;
+        _injectNomoAuthHeaders({ nomoAuthAddress, url: args.url, headers });
         res = await fetch(args.url, {
             method: args.method,
-            headers: Object.assign(Object.assign({}, injectedHeaders), args.headers),
+            headers,
             body: args.body,
         });
         statusCode = res.status;

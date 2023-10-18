2ef1ec3e57beff540cf6c11b620acdcdbfecf049
nomo-app <dev0@nomo.app>
2023-10-18 14:50:32 +0200

    remove exports that are already in nomo object

--- nomo-plugin-kit/src/index.ts
+++ nomo-plugin-kit/src/index.ts
@@ -1,25 +1,9 @@
 export { isFallbackModeActive } from './dart_interface';
 export { invokeNomoFunction } from './dart_interface';
 
-export { nomoLocalStorage } from './nomo_api';
 export { nomo } from './nomo_api';
 export { nomoConsole } from './nomo_api';
-export { nomoEnableMobileConsoleDebugging } from './nomo_api';
-export { nomoSignEvmTransaction } from './nomo_api';
-export { nomoSignEvmMessage } from './nomo_api';
-export { nomoGetPlatformInfo } from './nomo_api';
-export { nomoGetMessengerAddress } from './nomo_api';
-export { nomoGetWalletAddresses } from './nomo_api';
-export { nomoInjectQRCode } from './nomo_api';
-export { nomoTakePicture } from './nomo_api';
-export { nomoPickFromGallery } from './nomo_api';
-export { nomoGetTheme } from './nomo_api';
-export { nomoGetDeviceHashes } from './nomo_api';
-export { nomoGetDeviceName } from './nomo_api';
-export { nomoAuthHttp } from './nomo_api';
-export { nomoSendAssets } from './nomo_api';
 
-export { nomoAuthFetch } from './nomo_auth';
 export { simulateNomoAuthHttp } from './nomo_auth';
 
 export { getCurrentNomoTheme } from './nomo_theming';

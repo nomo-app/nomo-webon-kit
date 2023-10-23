b6811aaefb7011f80d1df51b2af8c13e248e746f
nomo-app <dev0@nomo.app>
2023-10-23 13:29:31 +0200

    deploy zip script

---
+++ demo-plugin/scripts/deploy_zip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+set -e
+
+RELEASE_FILE=nomo-demo-plugin.zip
+DEPLOY_PATH=/var/www/html/downloads/plugins/$RELEASE_FILE
+
+if [[ -n "$SSH_TARGET" ]]; then
+    echo "Deploying to SSH_TARGET: $SSH_TARGET"
+else
+    echo "Error: The environment variable SSH_TARGET is not set."
+    exit -1
+fi
+
+ls -l $RELEASE_FILE # ls to check if release file exists
+scp $RELEASE_FILE $SSH_TARGET:$DEPLOY_PATH
+
+./scripts/clear_cloudflare_cache.sh

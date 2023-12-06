4acffc49044a104dea9286bf19d185c625bc8cb5
nomo-fe <dev0@nomo.app>
2023-12-06 20:46:21 +0100

    demo-fix: send token to yourself instead of burning tokens

--- demo-webon/src/app/assets/send_demo.ts
+++ demo-webon/src/app/assets/send_demo.ts
@@ -5,9 +5,10 @@ export async function sendOnePercentOfBalance() {
   const asset = res.selectedAsset;
   const balance = BigInt(asset.balance);
   const amount = balance / 100n;
+  const ownAddress = await nomo.getEvmAddress();
   await nomo.sendAssets({
     asset,
-    targetAddress: "0x7561DEAf4ECf96dc9F0d50B4136046979ACdAD3e",
+    targetAddress: ownAddress,
     amount: amount.toString(), // must be in the smallest unit (e.g. wei or satoshi)
   });
 }

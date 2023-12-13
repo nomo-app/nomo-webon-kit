d0583f70225282635f9b6e68263901d7d2efb6c5
The Nomo App's core developer team <142992428+nomo-app@users.noreply.github.com>
2023-12-13 14:07:19 +0100

    add an npm package for ethers.js-V5 (in addition to the ethers.js-V6 package) (#85)

---
+++ ethersjs-nomo-webons-v5/dist/ethersjs_provider.js
@@ -0,0 +1,17 @@
+import { ethers } from "ethers";
+import { EthersjsNomoSigner } from "./ethersjs_nomo_signer";
+const rpcUrlZeniqSmartChain = "https://smart.zeniq.network:9545";
+const chainIdZeniqSmartChain = 383414847825;
+export const zscProvider = new ethers.providers.JsonRpcProvider(rpcUrlZeniqSmartChain, chainIdZeniqSmartChain);
+export const zscSigner = new EthersjsNomoSigner(zscProvider);
+export async function sendDemoTransaction() {
+    const ownAddress = await zscSigner.getAddress();
+    const value = ethers.utils.parseUnits("0.1", 18);
+    const tx = {
+        to: ownAddress,
+        value,
+        gasLimit: 21000,
+    };
+    const res = await zscSigner.sendTransaction(tx);
+    return res;
+}

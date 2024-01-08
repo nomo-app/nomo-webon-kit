6910ea955f28813adfe713849c92f9b97d95bd0e
nomo-fe <dev0@nomo.app>
2024-01-08 17:10:00 +0100

    migrate dev-WebOn from Next.js to Vite.js

--- demo-webon/nomo_cli.config.cjs
+++ demo-webon/nomo_cli.config.cjs
@@ -1,10 +1,17 @@
 const nomoCliConfig = {
   deployTargets: {
-    production: {
+    production1: {
       rawSSH: {
         sshHost: process.env.SSH_TARGET,
         sshBaseDir: "/var/www/production_webons/demowebon/",
         publicBaseUrl: "https://w.nomo.app/demowebon",
+      },
+    },
+    production2: {
+      rawSSH: {
+        sshHost: process.env.SSH_TARGET,
+        sshBaseDir: "/var/www/production_webons/devwebon/",
+        publicBaseUrl: "https://w.nomo.app/devwebon",
       },
     },
   },
--- demo-webon/src/app/page.tsx
+++ demo-webon/src/app/page.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-"use client";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import Dialog, { DialogContent } from "./components/dialog";
 import { useNomoState } from "./hooks/custom_hooks";
@@ -8,6 +5,7 @@ import { nomo } from "nomo-webon-kit";
 import { injectNomoCSSVariables } from "nomo-webon-kit";
 import styles from "./page.module.css";
 import "./nomo.css";
+import "./globals.css";
 import { testSigning } from "../../test/web3_signing_test";
 import { stringifyWithBigInts } from "nomo-webon-kit";
 import { mintNFT } from "./evm/mint_nft";
@@ -72,7 +70,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Nomo Demo WebOn - Get started by editing src/app/page.tsx. Scroll down
+          Nomo Dev WebOn - Scroll down
           to explore features of WebOns!
         </p>
       </div>
@@ -80,13 +78,12 @@ export default function Home() {
       <div className={styles.flex}>
         <Dialog content={dialog} handleClose={() => setDialog(null)} />
         <div style={{ height: "10px" }} />
-        <Image
+        <img
           className={styles.logo}
           src="/nomo-logo-square.jpg"
           alt="NOMO Logo"
           width={180}
           height={37}
-          priority
         />
         <div style={{ height: "10px" }} />
         <div style={{ width: "100%" }}>

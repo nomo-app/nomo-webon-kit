7620e176d717e3513fb82991039eabe8a77a5fe1
nomo-fe <dev0@nomo.app>
2024-02-21 14:30:08 +0100

    demo-WebOn: update the cli-config to do hybrid deployments

--- demo-webon/nomo_cli.config.cjs
+++ demo-webon/nomo_cli.config.cjs
@@ -5,6 +5,7 @@ const nomoCliConfig = {
         sshHost: process.env.SSH_TARGET,
         sshBaseDir: "/var/www/production_webons/demowebon/",
         publicBaseUrl: "https://w.nomo.app/demowebon",
+        hybrid: true,
       },
     },
     production2: {
@@ -12,6 +13,7 @@ const nomoCliConfig = {
         sshHost: process.env.SSH_TARGET,
         sshBaseDir: "/var/www/production_webons/devwebon/",
         publicBaseUrl: "https://w.nomo.app/devwebon",
+        hybrid: true,
       },
     },
   },

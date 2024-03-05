a99f67119bdf9f810402aff312752e29e63de5f9
The Nomo App's core developer team <142992428+nomo-app@users.noreply.github.com>
2024-03-05 15:52:57 +0100

    add support for cache signatures (#95)
    * add support for cache sigs
    
    * diff fix
    
    ---------
    
    Co-authored-by: nomo-fe <dev2@nomo.app>

--- demo-webon/nomo_cli.config.cjs
+++ demo-webon/nomo_cli.config.cjs
@@ -1,3 +1,10 @@
+require('dotenv').config();
+
+const mnemonic = process.env.CACHE_SIGN_MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please configure a CACHE_SIGN_MNEMONIC in a .env file");
+}
+
 const nomoCliConfig = {
   deployTargets: {
     production1: {
@@ -6,6 +13,7 @@ const nomoCliConfig = {
         sshBaseDir: "/var/www/production_webons/demowebon/",
         publicBaseUrl: "https://w.nomo.app/demowebon",
         hybrid: true,
+        mnemonic,
       },
     },
     production2: {
@@ -14,6 +22,7 @@ const nomoCliConfig = {
         sshBaseDir: "/var/www/production_webons/devwebon/",
         publicBaseUrl: "https://w.nomo.app/devwebon",
         hybrid: true,
+        mnemonic,
       },
     },
   },

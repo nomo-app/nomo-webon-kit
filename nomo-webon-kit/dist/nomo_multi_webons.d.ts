10671662e4c90a38abe46427fc122982c1c6379f
nomo-fe <dev2@nomo.app>
2024-03-05 15:36:08 +0100

    diff fix

--- nomo-webon-kit/dist/nomo_multi_webons.d.ts
+++ nomo-webon-kit/dist/nomo_multi_webons.d.ts
@@ -52,6 +52,11 @@ export interface NomoManifest {
      * cache_url should be a relative path.
      */
     cache_url?: string;
+    /**
+     * If set, the Nomo App will reject a cache if the signature cannot be verified.
+     * cache_sig should be an Ethereum-styled message signature of a tar.gz-cache.
+     */
+    cache_sig?: string;
 }
 /**
  * Returns the nomo_manifest.json that was used during the installation of the WebOn.

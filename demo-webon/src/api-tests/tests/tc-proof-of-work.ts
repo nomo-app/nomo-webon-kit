b1af1b24d0165cc4856a1418d0e7d124560a1a77
nomo-fe <dev2@nomo.app>
2024-10-15 16:18:40 +0200

    test: add hashing tests

--- demo-webon/src/api-tests/tests/tc-proof-of-work.ts
+++ demo-webon/src/api-tests/tests/tc-proof-of-work.ts
@@ -1,4 +1,4 @@
-import { nomo } from "nomo-webon-kit";
+import { invokeNomoFunction, nomo } from "nomo-webon-kit";
 import { NomoTest } from "../test-kit/nomo-test";
 
 class ProofOfWorkTest3Seconds extends NomoTest {
@@ -10,6 +10,10 @@ class ProofOfWorkTest3Seconds extends NomoTest {
   }
 
   async run() {
+    const res = await invokeNomoFunction("nomoSha256", {value: "hello world"});
+    if (res.digestHex !== "0039c7bb714b2f34be028cabe1da99250ff3f704bc38487e693c02228da7429b") {
+      throw new Error("Unexpected digestHex: " + res.digestHex);
+    }
     await nomo.proofOfWork({
       challenge: "0FDA",
       shaInputPrefix: "ui-test-" + Date.now(),
